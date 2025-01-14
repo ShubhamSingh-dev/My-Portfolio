695a2cc5bde8598588f7612cadd382a8fbe1f99e
Shubham Singh <mine.shubhamsingh@gmail.com>
2025-01-14 17:05:11 +0530

    Completion of Nvabar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 
 const Navbar = () => {
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
+  };
+
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+  };
   return (
     <>
       <div className="fixed top-0 left-0 right-0 w-11/12 -z-10">
@@ -57,12 +66,50 @@ const Navbar = () => {
             <Image src={assets.arrow_icon} className="w-3" alt="arrow" />
           </a>
 
-          <button className="block md:hidden ml-3">
+          <button className="block md:hidden ml-3" onClick={openMenu}>
             <Image src={assets.menu_black} alt="" className="w-6" />
           </button>
         </div>
 
         {/* -------- mobile menu -------- */}
+
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-slate-50 transition duration-500"
+        >
+          <div className="absolute right-6 top-6 " onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer "
+            />
+          </div>
+          <li>
+            <a className="font-Ovo" href="#top" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#about" onClick={closeMenu}>
+              About me
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#services" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#work" onClick={closeMenu}>
+              My Work
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#contact" onClick={closeMenu}>
+              Contact me
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
